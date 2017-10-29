8af20d2811a2696024df5f13d3bb9cddbea04e7d
BomBomBaster <igalhamer@gmail.com>
2017-10-29 07:28:31 +0200

    Initial commit

---
+++ app/src/main/java/com/igal/letstalk/HomeDiscussions.java
@@ -0,0 +1,13 @@
+package com.igal.letstalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeDiscussions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.discussion_list_screen);
+    }
+}

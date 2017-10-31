5b84e8bfa6b052deb9128845ae17a93204b2632f
BomBomBaster <igalhamer@gmail.com>
2017-10-31 11:59:56 +0200

    added adapter for gridview in home screen added adapter for listview in home screen

--- app/src/main/java/com/igal/letstalk/HomeDiscussions.java
+++ app/src/main/java/com/igal/letstalk/HomeDiscussions.java
@@ -2,6 +2,8 @@ package com.igal.letstalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
 
 public class HomeDiscussions extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class HomeDiscussions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.discussion_list_screen);
+
+        GridView options = (GridView) findViewById(R.id.search_options_gridview);
+        HomeOptionsAdapter homeOptionsAdapter = new HomeOptionsAdapter(this);
+
+        options.setAdapter(homeOptionsAdapter);
+
+        ListView discussions = (ListView) findViewById(R.id.listview_discussion);
+        HomeDiscussionsAdapter homeDiscussionsAdapter = new HomeDiscussionsAdapter(this);
+
+        discussions.setAdapter(homeDiscussionsAdapter);
     }
 }

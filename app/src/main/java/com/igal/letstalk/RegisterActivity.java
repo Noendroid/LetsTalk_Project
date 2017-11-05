a178fd4a07b2715fc401cb5dd72f8a7cbae6dd25
Noendroid <igalhamer@gmail.com>
2017-11-05 18:32:16 +0200

    register design fix register cancel button onClickListener

--- app/src/main/java/com/igal/letstalk/RegisterActivity.java
+++ app/src/main/java/com/igal/letstalk/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.igal.letstalk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_screen);
+
+        Button cancel = (Button) findViewById(R.id.register_button_cancel);
+        Button register = (Button) findViewById(R.id.register_button_confirm);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  register function HERE
+            }
+        });
     }
 }
